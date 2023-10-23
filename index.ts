653fd964fa5ccb25448b2bf615369297f0541991
alejandroP03 <d.pulidob@uniandes.edu.co>
2023-10-22 23:57:35 -0500

    Taller 2

--- index.ts
+++ index.ts
@@ -1,38 +1,61 @@
-import { series } from "./data.js";
-import Serie from "./serieEntity.js";
-
+import { series } from './data.js';
+import Serie from './serieEntity.js';
 
 function createSeries(series: Array<Serie>): void {
-    const list = document.getElementById("series_info");
-    if (!list) {
-        throw new Error("List not found");
-    }
+	const list = document.getElementById('series_info');
+	if (!list) {
+		throw new Error('List not found');
+	}
 
-    let seasonAverage: number = 0;
-    
-    series.forEach((serie) => {
-        const row = document.createElement("tr");
-        const aLink: HTMLElement = document.createElement("a");
-        aLink.setAttribute("href", serie.url);
-        aLink.appendChild(document.createTextNode(serie.name));
+	let seasonAverage: number = 0;
 
-        row.insertCell().appendChild(aLink);
-        row.insertCell().appendChild(document.createTextNode(serie.channel));
-        row.insertCell().appendChild(document.createTextNode(serie.seasons.toString()));
-        list.appendChild(row);
+	series.forEach(serie => {
+		const row = document.createElement('tr');
+		const aLink: HTMLElement = document.createElement('a');
+		aLink.setAttribute('href', serie.url);
+		aLink.appendChild(document.createTextNode(serie.name));
 
-        seasonAverage += serie.seasons;
-    });
+		row.insertCell().appendChild(aLink);
+		row.insertCell().appendChild(document.createTextNode(serie.channel));
+		row.insertCell().appendChild(
+			document.createTextNode(serie.seasons.toString())
+		);
 
-    seasonAverage /= series.length;
+		row.addEventListener('click', () => createCard(serie));
 
-    const seasonAvg: HTMLElement = document.getElementById("season_average")!;
-    seasonAvg.appendChild(document.createTextNode(seasonAverage.toString()));
+		list.appendChild(row);
+
+		seasonAverage += serie.seasons;
+	});
+
+	seasonAverage /= series.length;
+
+	const seasonAvg: HTMLElement = document.getElementById('season_average')!;
+	seasonAvg.appendChild(document.createTextNode(seasonAverage.toString()));
 }
 
-
-window.addEventListener("load", () => {
-    console.log("Page loaded");
-    createSeries(series);
-});
+function createCard(serie: Serie) {
+    const card = document.getElementById('card')!;
+	const cardTitle = document.getElementById('card-title')!;
+	const cardImage = document.getElementById('card-img')!;
+	const cardDescription = document.getElementById('card-description')!;
+	const cardLink = document.getElementById('card-link')!;
     
+	card.setAttribute("src", "serie.image");
+    
+	cardTitle.innerText = serie.name;
+	cardImage.setAttribute('src', serie.image);
+	cardDescription.innerText = serie.description;
+	cardLink.setAttribute('href', serie.url);
+	cardLink.innerText = serie.url;
+
+	cardImage.onload = () => {
+		card.classList.remove('visually-hidden');
+	};
+	
+}
+
+window.addEventListener('load', () => {
+	console.log('Page loaded');
+	createSeries(series);
+});
--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,43 @@
-import { series } from "./data.js";
+import { series } from './data.js';
 function createSeries(series) {
-    var list = document.getElementById("series_info");
+    var list = document.getElementById('series_info');
     if (!list) {
-        throw new Error("List not found");
+        throw new Error('List not found');
     }
     var seasonAverage = 0;
     series.forEach(function (serie) {
-        var row = document.createElement("tr");
-        var aLink = document.createElement("a");
-        aLink.setAttribute("href", serie.url);
+        var row = document.createElement('tr');
+        var aLink = document.createElement('a');
+        aLink.setAttribute('href', serie.url);
         aLink.appendChild(document.createTextNode(serie.name));
         row.insertCell().appendChild(aLink);
         row.insertCell().appendChild(document.createTextNode(serie.channel));
         row.insertCell().appendChild(document.createTextNode(serie.seasons.toString()));
+        row.addEventListener('click', function () { return createCard(serie); });
         list.appendChild(row);
         seasonAverage += serie.seasons;
     });
     seasonAverage /= series.length;
-    var seasonAvg = document.getElementById("season_average");
+    var seasonAvg = document.getElementById('season_average');
     seasonAvg.appendChild(document.createTextNode(seasonAverage.toString()));
 }
-window.addEventListener("load", function () {
-    console.log("Page loaded");
+function createCard(serie) {
+    var card = document.getElementById('card');
+    var cardTitle = document.getElementById('card-title');
+    var cardImage = document.getElementById('card-img');
+    var cardDescription = document.getElementById('card-description');
+    var cardLink = document.getElementById('card-link');
+    card.setAttribute("src", "serie.image");
+    cardTitle.innerText = serie.name;
+    cardImage.setAttribute('src', serie.image);
+    cardDescription.innerText = serie.description;
+    cardLink.setAttribute('href', serie.url);
+    cardLink.innerText = serie.url;
+    cardImage.onload = function () {
+        card.classList.remove('visually-hidden');
+    };
+}
+window.addEventListener('load', function () {
+    console.log('Page loaded');
     createSeries(series);
 });
